5cf069a40e51b33be2219cc7dbb9c4f92a577416
kWAY <55437151+kWAYTV@users.noreply.github.com>
2022-08-16 13:31:34 +0200

    Update b.py

--- b.py
+++ b.py
@@ -10,6 +10,10 @@ threads = input(f"Threads?: ")
 os.system(f'title Discord Vanity Checker ^- Threads: ' + str(threads) + ' - Ready!')
 clear()
 count = 0
+free = 0
+taken = 0
+ratelimited = 0
+blocked = 0
 proxyDebug = False
 
 # Vanity Generator Logo
@@ -25,7 +29,7 @@ def printLogo():
         print(Center.XCenter(Colorate.Horizontal(Colors.white_to_green, logo, 1)))
 
 def check():
-    global count
+    global count, free, taken, ratelimited, blocked
     while True:
         for vanity in vanitys:
             proxy = random.choice(open("proxies.txt","r").read().splitlines()); proxyDict = {"http": f"http://{proxy}"}
@@ -34,28 +38,25 @@ def check():
             else:
                 pass
             r = requests.get(f'https://discord.com/api/v9/invites/{vanity}', proxies=proxyDict)
+            count += 1
             if 'vanity_url_code' in (r.text):
-                count +=1
+                taken += 1
                 print(f"{Fore.RED}[{Fore.RESET}-{Fore.RED}] {Fore.RESET}Taken: " + vanity + " - Checked: " +str(count))
-                os.system(f'title Discord Vanity Checker ^- Checking: ' + vanity + ' ^- Status: taken')
                 with open('taken.txt', 'a') as f:
                     f.write(vanity + '\n')
             if 'Unknown Invite' in (r.text):
-                count +=1
+                free += 1
                 print(f"{Fore.GREEN}[{Fore.RESET}+{Fore.GREEN}] {Fore.RESET}Free/Termed: "+ vanity + " - Checked: " +str(count))
-                os.system(f'title Discord Vanity Checker ^- Checking: ' + vanity + ' ^- Status: avail/termed')
                 with open('free.txt', 'a') as f:
                     f.write(vanity + '\n')
             if 'Access denied' in (r.text):
-                count +=1
+                blocked += 1
                 print(f"{Fore.RED}[{Fore.RESET}-{Fore.RED}] {Fore.RESET}Cloudfare has blocked this IP.")
-                os.system(f'title Discord Vanity Checker ^- Checking: ' + vanity + ' ^- Status: Cloudfare Blocked - Checked: ' +str(count))
                 with open('blocked.txt', 'a') as f:
                     f.write(vanity + '\n')
             if 'retry_after' in (r.text):
-                count +=1
+                ratelimited += 1
                 print(f"{Fore.RED}[{Fore.RESET}-{Fore.RED}] {Fore.RESET}Rate limited, please wait.")
-                os.system(f'title Discord Vanity Checker ^- Checking: ' + vanity + ' ^- Status: Rate Limited - Checked: ' +str(count))
                 with open('ratelimited.txt', 'a') as f:
                     f.write(vanity + '\n')
 
