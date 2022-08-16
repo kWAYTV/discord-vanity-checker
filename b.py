78d08479887e61be15fafa4568e8fe1605bc527b
kWAY <55437151+kWAYTV@users.noreply.github.com>
2022-08-16 12:21:56 +0200

    Update b.py

--- b.py
+++ b.py
@@ -1,5 +1,6 @@
 import requests, os, threading, random, time
 from colorama import Fore, Back, Style
+from pystyle import Colors, Colorate, Center
 
 clear = lambda: os.system("cls" if os.name in ("nt", "dos") else "clear") # Don't touch this
 vanitys = open('check.txt', 'r').read().split('\n')
@@ -9,13 +10,29 @@ threads = input(f"Threads?: ")
 os.system(f'title Discord Vanity Checker ^- Threads: ' + str(threads) + ' - Ready!')
 clear()
 count = 0
+proxyDebug = False
+
+# Vanity Generator Logo
+logo = """
+██╗░░░██╗░█████╗░███╗░░██╗██╗████████╗██╗░░░██╗  ░█████╗░██╗░░██╗███████╗░█████╗░██╗░░██╗██████╗░
+██║░░░██║██╔══██╗████╗░██║██║╚══██╔══╝╚██╗░██╔╝  ██╔══██╗██║░░██║██╔════╝██╔══██╗██║░██╔╝██╔══██╗
+╚██╗░██╔╝███████║██╔██╗██║██║░░░██║░░░░╚████╔╝░  ██║░░╚═╝███████║█████╗░░██║░░╚═╝█████═╝░██████╔╝
+░╚████╔╝░██╔══██║██║╚████║██║░░░██║░░░░░╚██╔╝░░  ██║░░██╗██╔══██║██╔══╝░░██║░░██╗██╔═██╗░██╔══██╗
+░░╚██╔╝░░██║░░██║██║░╚███║██║░░░██║░░░░░░██║░░░  ╚█████╔╝██║░░██║███████╗╚█████╔╝██║░╚██╗██║░░██║
+░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░░╚═╝░░░░░░╚═╝░░░  ░╚════╝░╚═╝░░╚═╝╚══════╝░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝"""
+
+def printLogo():
+        print(Center.XCenter(Colorate.Horizontal(Colors.white_to_green, logo, 1)))
 
 def check():
     global count
     while True:
         for vanity in vanitys:
             proxy = random.choice(open("proxies.txt","r").read().splitlines()); proxyDict = {"http": f"http://{proxy}"}
-            print(f"{Fore.MAGENTA}[{Fore.RESET}!{Fore.MAGENTA}] {Fore.RESET}Using proxy: {Fore.GREEN}{proxyDict}{Fore.RESET}")
+            if proxyDebug == True:
+                print(f"{Fore.MAGENTA}[{Fore.RESET}!{Fore.MAGENTA}] {Fore.RESET}Using proxy: {Fore.GREEN}{proxyDict}{Fore.RESET}")
+            else:
+                pass
             r = requests.get(f'https://discord.com/api/v9/invites/{vanity}', proxies=proxyDict)
             if 'vanity_url_code' in (r.text):
                 count +=1
@@ -43,6 +60,7 @@ def check():
                     f.write(vanity + '\n')
 
 clear()
+printLogo()
 try:
     while True:
         if threading.active_count() < int(threads):
